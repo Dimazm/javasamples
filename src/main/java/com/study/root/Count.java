79be2d81a723242152f80b86d4c9d5fbde8be7a5
dimazm <dimazm@live.com>
2017-03-30 11:34:55 +0300

    Exs1

--- src/main/java/com/study/root/Count.java
+++ src/main/java/com/study/root/Count.java
@@ -4,23 +4,27 @@ package com.study.root;
  * Created by dzzm1214 on 29.03.2017.
  */
 public class Count {
+
+    public static final double ONE_METER = 39.37;
+
     public static void main(String[] args) {
 
         double m;
-        int d;
         int count;
         count = 0;
 
-        for (d = 1; d <= 100; d++) {
-            m = d / 39.37;
+        for (int d = 1; d <= 100; d++) {
+
+            m = d / ONE_METER;
+
             System.out.println(d + " Дюймов" + " = " + m + " Meters");
 
             count++;
-            if (count == 12)
-            {
-                System.out.println();
-
-            count = 0;}
+            if (count == 11) {
+                double f = d / 12;
+                System.out.println(f + "Футов");
+                count = 0;
+            }
 
         }
     }
