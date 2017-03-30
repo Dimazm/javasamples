d73c7eab7cbe4f138fbe6b5283c650de50b452aa
Dzmitry Zmachynski <dzzm1214@netcracker.com>
2017-03-30 11:09:22 +0300

    Add Project

---
+++ src/main/java/com/study/root/App.java
@@ -0,0 +1,21 @@
+package com.study.root;
+
+import java.awt.*;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+class App {
+    public static void main(String[] args) {
+        double a, l;
+
+        a = 82; // мой вес
+        l = 17 * a / 100;
+
+        System.out.println("Мой вес на Земле =" + a +" кг\n" + "Мой вес на Луне = " + l+" кг\n");
+
+
+    }
+}
---
+++ src/main/java/com/study/root/Count.java
@@ -0,0 +1,31 @@
+package com.study.root;
+
+/**
+ * Created by dzzm1214 on 29.03.2017.
+ */
+public class Count {
+    public static void main(String[] args) {
+
+        double m;
+        int d;
+        int count;
+        count = 0;
+
+        for (d = 1; d <= 100; d++) {
+            m = d / 39.37;
+            System.out.println(d + " Дюймов" + " = " + m + " Meters");
+
+            count++;
+
+            if (count == 12)
+            {
+                System.out.println();
+
+            count = 0;}
+
+        }
+    }
+}
+
+
+
