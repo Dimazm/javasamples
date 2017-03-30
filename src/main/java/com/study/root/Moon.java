90cdd9b65b3b074d3da28ce58c582056f9ae7e5e
dimazm <dimazm@live.com>
2017-03-30 17:48:10 +0300

    Задача № 2 ot full

---
+++ src/main/java/com/study/root/Moon.java
@@ -0,0 +1,18 @@
+package com.study.root;
+
+/**
+ * Created by dzzm1214 on 30.03.2017.
+ */
+public class Moon {
+
+    public static void main(String[] args) {
+        double a, l;
+
+        a = 82; // мой вес
+        l = 17 * a / 100;
+
+        System.out.println("Мой вес на Земле =" + a +" кг\n" + "Мой вес на Луне = " + l+" кг\n");
+
+
+    }
+}
